290aafca485792fbddd8b1039d8310c3b4378c02
kun huang <huangkunsmail@gmail.com>
2020-01-07 18:51:42 -0500

    team_heros
    ban/pick total

--- supp.py
+++ supp.py
@@ -7,6 +7,7 @@ Created on Thu Jan  2 15:17:36 2020
 
 import requests
 import json
+import pandas as pd
 
 def team_heros(team_id, ver_time):
     """Summary heros a team uses after a specific version
@@ -16,13 +17,7 @@ def team_heros(team_id, ver_time):
         ver_time: Unix timestamp when the patch is released, convert from https://www.unixtimestamp.com/
     
     Returns:
-        A dict mapping keys to the corresponding table row data fetched.
-        Each row is represented as a tuple of integer(wins, loses, winrate).
-        For example:
-        
-        ['lich':(9, 1, 0.9),
-         ...
-        ]
+        A DataFrame containing heros and their ban/pick totals
     """
     r = requests.get('https://api.opendota.com/api/teams/{}/matches'.format(team_id))
     match = json.loads(r.text)
@@ -30,4 +25,39 @@ def team_heros(team_id, ver_time):
     for m in match:
         if m['start_time'] > ver_time: # Unix timestamp, after 7.23
             match_id.append(m['match_id'])
-    return match_id
+    heros = json.loads(requests.get('https://api.opendota.com/api/heroes').text)
+    for hero in heros:
+        hero.update({'pick_total':0,
+                     'ban_total':0})
+    heros_own = {}
+    heros_against = {}
+    for i in range(len(heros)):
+        heros_own[heros[i]['id']] = heros[i].copy()
+        heros_against[heros[i]['id']] = heros[i].copy()
+
+    for mid in match_id:
+        match_data = json.loads(requests.get('https://api.opendota.com/api/matches/{}'.format(mid)).text) 
+        # get the position of team_id
+        # pos = 1: dire, pos = 0: radiant
+        if match_data['dire_team']['team_id'] == team_id:
+            pos = 1
+        else:
+            pos = 0
+        # bp: 22 list, containing bp data
+        bp = match_data['picks_bans']
+        for s in bp:
+            if s['team'] == pos:
+                if s['is_pick']:
+                    heros_own[s['hero_id']]['pick_total'] += 1
+                else:
+                    heros_own[s['hero_id']]['ban_total'] += 1
+            else:
+                if s['is_pick']:
+                    heros_against[s['hero_id']]['pick_total'] += 1
+                else:
+                    heros_against[s['hero_id']]['ban_total'] += 1
+    own = pd.DataFrame(heros_own)
+    against = pd.DataFrame(heros_against)
+    return [own.T, against.T]
+
+#m = team_heros(15, 1574812800)
