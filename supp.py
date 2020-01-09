b24a56920e51765eb7c8b92b1affcb40bdc0ed69
kun huang <huangkunsmail@gmail.com>
2020-01-09 17:40:04 -0500

    team_pick_winrate need improvement

--- supp.py
+++ supp.py
@@ -115,4 +115,68 @@ def team_match_summary(team_id, ver_time):
 
 
 # t = team_match_summary(726228, 1574812800)
+def team_pick_winrate(team_id, ver_time):
+    """summary match details of the team from ver_time
 
+    Args:
+        team_id: team id of a Dota2 team, can get from https://www.opendota.com/
+        ver_time: Unix timestamp when the patch is released, convert from https://www.unixtimestamp.com/
+
+    Returns:
+        A DataFrame [against, league, teamfight_total, gold_adv_10, xp_adv_10, win]
+    """
+    r = requests.get('https://api.opendota.com/api/teams/{}/matches'.format(team_id))
+    match = json.loads(r.text)
+    match_id = []
+    for m in match:
+        if m['start_time'] > ver_time:  # Unix timestamp
+            match_id.append(m['match_id'])
+    heros1 = json.loads(requests.get('https://api.opendota.com/api/heroes').text)
+    heros2 = json.loads(requests.get('https://api.opendota.com/api/heroes').text)  # avoid referring in dict
+
+    for i in range(len(heros1)):
+        del heros1[i]['name'], heros1[i]['primary_attr'], heros1[i]['attack_type'], heros1[i]['legs']
+        del heros2[i]['name'], heros2[i]['primary_attr'], heros2[i]['attack_type'], heros2[i]['legs'], heros2[i][
+            'roles']
+        heros2[i].update({'pick_total': 0, 'win_total': 0, 'loss_total': 0})
+
+    heros_all = {}
+    heros_comb = {}
+    for i in range(len(heros1)):
+        heros_all[heros1[i]['id']] = heros1[i].copy()
+        heros_comb[heros2[i]['id']] = heros2[i].copy()
+        heros_all[heros1[i]['id']]['with'] = heros_comb
+
+    for hero_id in heros_all:
+        for mid in match_id:
+            match_data = json.loads(requests.get('https://api.opendota.com/api/matches/{}'.format(mid)).text,
+                                    encoding='utf-8')
+            # get the position of team_id
+            # pos = 1: dire, pos = 0: radiant
+            win = True
+            if match_data['dire_team']['team_id'] == team_id:
+                pos = 1
+                win = not (match_data['radiant_win'])
+            else:
+                pos = 0
+                win = match_data['radiant_win']
+
+            # bp: 22 list, containing bp data
+            bp = match_data['picks_bans']
+            for s in bp:
+                flag = False
+                if s['hero_id'] == hero_id and s['team'] == pos and s['is_pick']:
+                    flag = True
+                    team_pick_hero_id = []
+                    for s2 in bp:
+                        if s2['team'] == pos and s2['is_pick']:
+                            team_pick_hero_id.append(s2['hero_id'])
+                    for hero_id_pick in team_pick_hero_id:
+                        heros_all[hero_id]['with'][hero_id_pick]['pick_total'] += 1
+                        if win:
+                            heros_all[hero_id]['with'][hero_id_pick]['win_total'] += 1
+                        else:
+                            heros_all[hero_id]['with'][hero_id_pick]['loss_total'] += 1
+                if flag:
+                    break
+    return heros_all
